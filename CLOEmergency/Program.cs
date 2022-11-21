5324e4b95a2de3a6188649affce0111dccb7426e
sunwoori <sunwoori@SUNWOORI-PC>
2022-11-21 23:29:51 +0900

    swaggerDoc 추가

--- CLOEmergency/Program.cs
+++ CLOEmergency/Program.cs
@@ -1,6 +1,6 @@
 using CLOEmergency;
-using CLOEmergency.Repository;
-using CLOEmergency.Services;
+using Microsoft.OpenApi.Interfaces;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,18 +8,38 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers(mvcOptions =>
 {
+    // Response.Body string formatter
     mvcOptions.InputFormatters.Add(new TextSingleValueFormatter());
 });
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Emergency API",
+        Description = "Emergency Employee Phone Book",
+        TermsOfService = new Uri("https://www.clo3d.com/ko/legal/terms-of-service"),
+        Contact = new OpenApiContact
+        {
+            Name = "CLO Contact Us",
+            Url = new Uri("https://www.clo3d.com/ko/support/contactus")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "CLO Plans",
+            Url = new Uri("https://www.clo3d.com/ko/plans")
+        }
+    });
+});
+
 
 
 builder.Services.AddScoped<IEmployeeService, EmployeeService>();
 builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
-
-
 
 var app = builder.Build();
 
