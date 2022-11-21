66fef7ba371b6022454fa715a12cccde60e13804
sunwoori <sunwoori@SUNWOORI-PC>
2022-11-21 23:31:31 +0900

    프로젝트 파일 수정

---
+++ CLOEmergency/Utils/Utils.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace CLOEmergency.Utils
+{
+    public static class Utils
+    {
+        public static JArray JsonHelper(string employee)
+        {
+            JArray jArray = new JArray();
+
+
+            return jArray;
+        }
+    }
+}
