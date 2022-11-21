d8ad747df2ffbaad9a811948bd35707106b94dcb
sunwoori <sunwoori@SUNWOORI-PC>
2022-11-21 23:29:01 +0900

    글로벌 using

---
+++ CLOEmergency/GlobalUsing.cs
@@ -0,0 +1,7 @@
+﻿global using System.Text;
+global using System.Web;
+global using CLOEmergency.Models;
+global using CLOEmergency.Repository;
+global using CLOEmergency.Services;
+global using Newtonsoft.Json;
+
